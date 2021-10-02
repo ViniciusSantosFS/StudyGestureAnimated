1ee003188c35a382f2857b8eb8e5995920384937
vinicius santos <viniciusantos.florencio@gmail.com>
2021-10-01 21:31:00 -0300

    Adicionando readme.md

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Practice project to learn about React-Native Animated and Gesture Handlers  </br>
+
+    Project consist: 
+        - a list of users data that came from an external public api
+        - cards with image, name and etc about user
+    
+
+    The main ideia behind the project is try combine Animated with Gesture Handlers to move only one single card per time
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { View, StyleSheet, PanResponder, Animated } from "react-native";
+import { PanResponder, Animated } from "react-native";
 
 export default function Deck({ data, renderCard }) {
   const animationCard = useRef(new Animated.ValueXY()).current;
@@ -7,14 +7,17 @@ export default function Deck({ data, renderCard }) {
   const panResponder = useRef(
     PanResponder.create({
       onPanResponderStart: () => true, // verifica se esse pan responder é responsavél pelo gesto
+
       onMoveShouldSetPanResponder: () => true,
+
       onPanResponderMove: (event, gestureState) => {
         animationCard.setValue({
           x: gestureState.dx,
           y: gestureState.dy,
         });
       }, // Verifica os gestos do usúario com a aplicação
-      onPanResponderRelease: () => {}, // usúario finalizou a interação com a aplicação
+
+      onPanResponderRelease: (event, gestureFinalState) => {}, // usúario finalizou a interação com a aplicação
     })
   ).current;
 
