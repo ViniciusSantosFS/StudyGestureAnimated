6ab4abdc8eccbe7f8fcabf68eee23d17ac8dbea5
vinicius santos <viniciusantos.florencio@gmail.com>
2021-10-01 21:00:09 -0300

    Adicionando animação com o gesture handler

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,12 +1,35 @@
 import React, { useRef } from "react";
-import { View, StyleSheet, Animated } from "react-native";
+import { View, StyleSheet, PanResponder, Animated } from "react-native";
 
 export default function Deck({ data, renderCard }) {
+  const animationCard = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onPanResponderStart: () => true, // verifica se esse pan responder é responsavél pelo gesto
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        animationCard.setValue({
+          x: gestureState.dx,
+          y: gestureState.dy,
+        });
+      }, // Verifica os gestos do usúario com a aplicação
+      onPanResponderRelease: () => {}, // usúario finalizou a interação com a aplicação
+    })
+  ).current;
+
   const renderCards = () => {
     return data.map((item) => {
       return renderCard(item);
     });
   };
 
-  return <View>{renderCards()}</View>;
+  return (
+    <Animated.View
+      {...panResponder.panHandlers}
+      style={animationCard.getLayout()}
+    >
+      {renderCards()}
+    </Animated.View>
+  );
 }
