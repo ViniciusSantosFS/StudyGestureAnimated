5e56de786ad4d64dd3eb4ee068b90b2f9d2dcd2a
vinicius santos <viniciusantos.florencio@gmail.com>
2021-10-04 15:49:24 -0300

    Ajuste no bug em que card não re renderizava para mudar seu index

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,37 +1,47 @@
-import React, { useRef } from "react";
+import React, { useRef, useMemo, useState } from "react";
 import { View, PanResponder, Animated, Dimensions } from "react-native";
 
 const CURRENT_SCREEN_WIDTH = Dimensions.get("window").width;
 const SWIPE_SUCCESS = 0.25 * CURRENT_SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
-export default function Deck({ data, renderCard }) {
+const defaultVoid = () => {};
+
+export default function Deck({
+  data,
+  renderCard,
+  onSwipedRight = defaultVoid,
+  onSwipedLeft = defaultVoid,
+}) {
+  const [currentCard, setCurrentCard] = useState(0);
   const animationCard = useRef(new Animated.ValueXY()).current;
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onPanResponderStart: () => true, // verifica se esse pan responder é responsavél pelo gesto
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onPanResponderStart: () => true, // verifica se esse pan responder é responsavél pelo gesto
 
-      onMoveShouldSetPanResponder: () => true,
+        onMoveShouldSetPanResponder: () => true,
 
-      onPanResponderMove: (event, gestureState) => {
-        animationCard.setValue({
-          x: gestureState.dx,
-          y: gestureState.dy,
-        });
-      }, // Verifica os gestos do usúario com a aplicação
+        onPanResponderMove: (event, gestureState) => {
+          animationCard.setValue({
+            x: gestureState.dx,
+            y: gestureState.dy,
+          });
+        }, // Verifica os gestos do usúario com a aplicação
 
-      onPanResponderRelease: (event, gestureFinalState) => {
-        if (gestureFinalState.dx > SWIPE_SUCCESS) {
-          forceSwipe("RIGHT");
-        } else if (gestureFinalState.dx < -SWIPE_SUCCESS) {
-          forceSwipe("left");
-        } else {
-          resetCardPosition();
-        }
-      }, // usúario finalizou a interação com a aplicação
-    })
-  ).current;
+        onPanResponderRelease: (event, gestureFinalState) => {
+          if (gestureFinalState.dx > SWIPE_SUCCESS) {
+            forceSwipe("RIGHT");
+          } else if (gestureFinalState.dx < -SWIPE_SUCCESS) {
+            forceSwipe("left");
+          } else {
+            resetCardPosition();
+          }
+        }, // usúario finalizou a interação com a aplicação
+      }),
+    [currentCard]
+  );
 
   const resetCardPosition = () => {
     Animated.spring(animationCard, {
@@ -55,7 +65,15 @@ export default function Deck({ data, renderCard }) {
   };
 
   const onSwiped = (direction) => {
-    direction === "right" ? onSwipedRight : onSwipedLeft; // Futuras funções
+    const item = data[currentCard];
+
+    direction.toLowerCase() === "right"
+      ? onSwipedRight(item)
+      : onSwipedLeft(item);
+    // Futuras funções
+    animationCard.setValue({ x: 0, y: 0 });
+
+    setCurrentCard(currentCard + 1);
   };
 
   const getStyles = () => {
@@ -71,7 +89,8 @@ export default function Deck({ data, renderCard }) {
 
   const renderCards = () => {
     return data.map((item, index) => {
-      if (index === 0) {
+      if (index < currentCard) return null;
+      if (index === currentCard) {
         return (
           <Animated.View
             key={item.login.uuid}
